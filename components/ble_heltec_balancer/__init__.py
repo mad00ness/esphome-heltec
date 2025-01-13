eac8d31062656e9b5fac24c6acb7241880b89c5d
mad00ness <mad00ness@gmail.com>
2025-01-13 17:45:35 +0300

    some 1

--- components/ble_heltec_balancer/__init__.py
+++ components/ble_heltec_balancer/__init__.py
@@ -1,6 +1,5 @@
-from esphome import automation
 import esphome.codegen as cg
-from esphome.components import ble_client, esp32_ble_tracker, sensor
+from esphome.components import ble_client
 import esphome.config_validation as cv
 from esphome.const import (
     DEVICE_CLASS_SIGNAL_STRENGTH,
@@ -8,13 +7,11 @@ from esphome.const import (
     UNIT_DECIBEL_MILLIWATT,
 )
 
-from .. import ble_client_ns
-
 DEPENDENCIES = ["ble_client"]
 
-TYPE_BALANCER = "balancer"
+MULTI_CONF = True
 
-CONF_HELTEC_BALANCER_BLE_ID = "heltec_balancer_ble_id"
+CONF_BLE_HELTEC_BALANCER_ID = "ble_heltec_balancer_id"
 
 heltec_balancer_ble_ns = cg.esphome_ns.namespace("ble_heltec_balancer")
 HeltecBalancerBle = heltec_balancer_ble_ns.class_(
@@ -23,29 +20,23 @@ HeltecBalancerBle = heltec_balancer_ble_ns.class_(
 
 HELTEC_BALANCER_BLE_COMPONENT_SCHEMA = cv.Schema(
     {
-        cv.GenerateID(CONF_HELTEC_BALANCER_BLE_ID): cv.use_id(HeltecBalancerBle),
+        cv.GenerateID(CONF_BLE_HELTEC_BALANCER_ID): cv.use_id(HeltecBalancerBle),
     }
 )
 
-CONFIG_SCHEMA = cv.All(
-    checkType,
-    cv.typed_schema(
+CONFIG_SCHEMA = (
+    cv.Schema(
         {
-            TYPE_BALANCER: sensor.sensor_schema(
-                HeltecBalancerBle,
-                accuracy_decimals=0,
-                unit_of_measurement=UNIT_DECIBEL_MILLIWATT,
-                device_class=DEVICE_CLASS_SIGNAL_STRENGTH,
-                state_class=STATE_CLASS_MEASUREMENT,
-            )
-            .extend(cv.polling_component_schema("60s"))
-            .extend(ble_client.BLE_CLIENT_SCHEMA),
-        },
-        lower=True,
-    ),
+            cv.GenerateID(): cv.declare_id(HeltecBalancerBle),
+        }
+    )
+    .extend(ble_client.BLE_CLIENT_SCHEMA)
+    .extend(cv.polling_component_schema("5s"))
 )
 
+
 async def to_code(config):
-    var = await sensor.new_sensor(config)
+    var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
     await ble_client.register_ble_node(var, config)
+
